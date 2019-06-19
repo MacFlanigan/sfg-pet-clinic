1ad2c4e55329cfe1c926b1a30511f456fab2fe1e
Nicolas Leclerc <nleclerc@sodifrance.fr>
2019-06-19 16:22:35 +0200

    fixed controllers links

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -4,8 +4,10 @@ import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("owners")
 public class OwnerController {
 
     private final OwnerService ownerService;
@@ -14,9 +16,14 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @GetMapping("owners")
+    @GetMapping("/")
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @GetMapping("find")
+    public String findOwners() {
+        return "notImplemented";
     }
 }
